b11d973abd1fcb3711702d91be86ee1d6f4b7e30
Jezreel Joshua <jezreeljoshuam@gmail.com>
2020-09-24 18:20:31 +0800

    enable username and password caching for now. disable on release

--- src/controllers/LoginPageController.java
+++ src/controllers/LoginPageController.java
@@ -169,8 +169,8 @@ public class LoginPageController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        usernameField.clear();
-        passwordField.clear();
+//        usernameField.clear();
+//        passwordField.clear();
 
         try {
             connectionClass = new ConnectionClass();
